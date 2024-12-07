71e88f36e8b2775b266b1e01e297799e1d667c68
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-07 19:40:46 +0100

    Add files via upload

--- CarsMethodsTask1/Program.cs
+++ CarsMethodsTask1/Program.cs
@@ -11,6 +11,9 @@ namespace CarsMethodsTask1
             ToyotaCorolla.SetYear(new DateTime(2023, 1, 1));
             ToyotaCorolla.DescribeCar();
 
+            ToyotaCorolla.IdNumber = "2541";
+            Console.WriteLine(ToyotaCorolla.IdNumber);
+
             Car MercedesBenz = new Car(1990, "Mercedes", "Benz");
             MercedesBenz.DescribeCar();
 
