87d0514cdec76ea6c13bb17babe26dbba9390af7
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-09 17:33:20 +0100

    Add files via upload

---
+++ AnimalsInZoo.Task1.C#/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace AnimalsInZoo
+{
+    public interface IZooAnimal
+    {
+        void MakeSound();
+        void Describe();
+    }
+
+    public class Lion : IZooAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Roar!");
+        }
+        public void Describe()
+        {
+            Console.WriteLine("This is a lion. It is the king of the jungle.");
+        }
+    }
+    public class Elephant : IZooAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Trumpet!");
+        }
+        public void Describe()
+        {
+            Console.WriteLine("This is an elephant. It is the largest land animal.");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<IZooAnimal> listofAnimal = new List<IZooAnimal>();
+            listofAnimal.Add(new Lion());
+            listofAnimal.Add(new Elephant());
+
+
+
+
+            foreach (IZooAnimal animal in listofAnimal)
+            {
+                animal.MakeSound();
+                animal.Describe();
+
+            }
+        }
+    }
+}
