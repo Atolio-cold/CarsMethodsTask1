97ed14dac38b655bd5264bc50a52c462cf4af3b7
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-09 17:48:05 +0100

    Add files via upload

--- AnimalsInZoo.Task1.C#/Program.cs
+++ AnimalsInZoo.Task1.C#/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.ComponentModel;
 
+
 namespace AnimalsInZoo
 {
     public interface IZooAnimal
@@ -33,6 +34,31 @@ namespace AnimalsInZoo
         }
     }
 
+    public class Tiger : IZooAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Wrrr!");
+        }
+        public void Describe()
+        {
+            Console.WriteLine("This is an Tiger.It is one of the fastest land animals.");
+
+        }
+
+    }
+    public class Monkey : IZooAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Uhuhuh!");
+        }
+        public void Describe()
+        {
+            Console.WriteLine("This is a Monkey. It is a funny and clever animal.");
+        }
+
+    }
     public class Program
     {
         public static void Main()
@@ -40,8 +66,8 @@ namespace AnimalsInZoo
             List<IZooAnimal> listofAnimal = new List<IZooAnimal>();
             listofAnimal.Add(new Lion());
             listofAnimal.Add(new Elephant());
-
-
+            listofAnimal.Add(new Tiger());
+            listofAnimal.Add(new Monkey());
 
 
             foreach (IZooAnimal animal in listofAnimal)
@@ -53,3 +79,4 @@ namespace AnimalsInZoo
         }
     }
 }
+
